3a3c3ca2b8c7a0eb07694ec1faf69a0ca7c4f670
Per <per.schulte@gmail.com>
2016-05-02 23:27:35 +0200

    Initial Commit

---
+++ ALS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALS
+//
+//  Created by Per Schulte on 02.05.16.
+//  Copyright © 2016 Per Schulte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
