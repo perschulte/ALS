63b97b22b098ff5a79832dd683af0d58218b2329
Per <per.schulte@gmail.com>
2016-05-13 07:11:10 +0200

    Added last used texts tableview

--- ALS/SpokenSnippet.swift
+++ ALS/SpokenSnippet.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class SpokenSnippet {
+    var text = ""
+    var lastUsed = NSDate()
+    
+    init(_ text: String) {
+        self.text = text
+    }
+}
+
--- ALS/ViewController.swift
+++ ALS/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 import AVFoundation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var tableView: UITableView!
     
     let speechSynthesizer = AVSpeechSynthesizer()
+    var spokenSnippets = [SpokenSnippet]()
     
-    @IBAction func speak(sender: UIButton) {
-        
-        let speechUtterance = AVSpeechUtterance(string: textView.text!)
+    @IBAction func pressSpeak(sender: UIButton) {
+        speak(textView!.text)
+    }
+    
+    func speak(text: String) {
+        speechSynthesizer.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        let speechUtterance = AVSpeechUtterance(string: text)
         speechUtterance.voice = AVSpeechSynthesisVoice(language: "de-DE")
         
         for voice in AVSpeechSynthesisVoice.speechVoices() {
@@ -28,7 +34,17 @@ class ViewController: UIViewController {
             }
         }
         speechSynthesizer.speakUtterance(speechUtterance)
+        
+        let snippet = textView!.text
+        if !spokenSnippets.contains({$0.text == snippet}) {
+            spokenSnippets.append(SpokenSnippet(snippet))
+            tableView.reloadData()
+            if spokenSnippets.count > 20 {
+                spokenSnippets.removeFirst()
+            }
+        }
     }
+    
     @IBAction func deleteText(sender: UIButton) {
         textView.text = nil
     }
@@ -36,6 +52,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
         
@@ -47,13 +64,28 @@ class ViewController: UIViewController {
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
             bottomConstraint.constant = 20 + keyboardSize.height
         }
-        
     }
     
     func keyboardWillHide(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
             bottomConstraint.constant = 20 - keyboardSize.height
         }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spokenSnippets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        cell.textLabel?.text = spokenSnippets[indexPath.row].text
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let snippet = spokenSnippets[indexPath.row]
+        speak(snippet.text)
+        snippet.lastUsed = NSDate()
     }
 }
 
